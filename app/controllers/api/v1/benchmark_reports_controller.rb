5c6c9b1770aec28a7a2ef19ec17803a3df2e4c05
Samuel Massinon <samuel.massinon@invenia.ca>
2016-03-30 09:34:34 -0500

    can now send post

--- app/controllers/api/v1/benchmark_reports_controller.rb
+++ app/controllers/api/v1/benchmark_reports_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::BenchmarkReportsController < ApplicationController
+    skip_before_action :verify_authenticity_token
     respond_to :json
 
     def index
@@ -7,9 +8,6 @@ class Api::V1::BenchmarkReportsController < ApplicationController
 
     def upload
         render text: "Thanks for sending a POST request with cURL! Payload: #{request.body.read}"
-
-        byebug
-
     end
 
 end
