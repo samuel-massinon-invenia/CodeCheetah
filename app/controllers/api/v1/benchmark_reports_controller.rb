c6cef876afc77e6fe8925ca71e0bb687b1d8aaf2
Samuel Massinon <samuel.massinon@invenia.ca>
2016-04-18 13:33:14 -0500

    Able to upload reports with a name

--- app/controllers/api/v1/benchmark_reports_controller.rb
+++ app/controllers/api/v1/benchmark_reports_controller.rb
@@ -7,6 +7,16 @@ class Api::V1::BenchmarkReportsController < ApplicationController
     end
 
     def upload
+        unless BenchmarkReport.find_by(name: params[:name]) == nil
+            throw("error")
+        end
+
+        report = BenchmarkReport.create(name: params[:name])
+
+        params[:results].each do |name, score|
+            BenchmarkResult.create(name: name, score: score, benchmark_report_id: report.id)
+        end
+
         render text: "Thanks for sending a POST request with cURL! Payload: #{request.body.read}"
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212025324) do
+ActiveRecord::Schema.define(version: 20160330021641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160212025324) do
     t.integer  "benchmark_repo_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.string   "name"
   end
 
   add_index "benchmark_reports", ["benchmark_repo_id"], name: "index_benchmark_reports_on_benchmark_repo_id", using: :btree
